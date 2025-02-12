db6120f48e06ff7db8f0bb8945480c96f2f7ecaa
Gabriele Canepa <git@gabrielecanepa.com>
2025-02-12 02:38:31 +0100

    chore: refactor utility packages

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,4 +1,5 @@
 import { readdirSync } from 'node:fs'
+
 import { RuleConfigSeverity, type UserConfig } from '@commitlint/types'
 
 const SCOPES = ['deps', 'deps-dev']
@@ -12,6 +13,7 @@ export default {
   defaultIgnores: true,
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'body-max-line-length': [RuleConfigSeverity.Disabled],
     'scope-enum': [RuleConfigSeverity.Error, 'always', [...SCOPES, ...packages]],
   },
 } satisfies UserConfig

b25a31c4946f8b0ebc84ef11d06861d3574bcdf5
Gabriele Canepa <git@gabrielecanepa.com>
2025-04-07 12:55:10 +0200

    style(utils): add type `PathParams` and comment all types

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -1,35 +1,95 @@
+/**
+ * Type or array of type.
+ */
 export type OrArray<T> = T | T[]
 
+/**
+ * Function with any arguments and return type.
+ */
 export type AnyFunction = (...args: any) => any
 
+/**
+ * Any object type with string keys.
+ */
 export type AnyObject = Record<string, any>
 
+/**
+ * Any array type, including nested arrays.
+ */
 export type AnyArray = any[] | any[][]
 
+/**
+ * Any type of object key.
+ */
 export type AnyKey = string | number | symbol
 
+/**
+ * Record of parameters extracted from segments of a string path.
+ * 
+ * @example
+ * ```ts
+ * type Params = PathParams<'/users/:id/posts/:postId'>
+ * // Result: { id: string; postId: string }
+ * ```
+ */
+export type PathParams<S extends string> = S extends `${infer _}:${infer Param}/${infer Rest}`
+  ? { [K in Param | keyof PathParams<Rest>]: string }
+  : S extends `${infer _}:${infer Param}`
+    ? Record<Param, string>
+    : AnyObject
+
+/**
+ * Partial record with non-nullable values.
+ */
 export type NonNullablePartial<T> = Partial<{
   [K in keyof T]: NonNullable<T[K]>
 }>
 
+/**
+ * Record with `null` values converted to `undefined`.
+ */
 export type NullToUndefined<T> = {
   [K in keyof T]: null extends T[K] ? NonNullable<T[K]> | undefined : T[K]
 }
 
+/**
+ * Type or null type.
+ */
 export type Nullable<T> = T | null
 
+/**
+ * Primitive type.
+ */
 export type Primitive = string | number | bigint | boolean | null | undefined
 
+/**
+ * Record with recursive values.
+ */
 export interface Recursive<T> {
   [key: string]: Recursive<T> | T
 }
 
+/**
+ * String or number type.
+ */
 export type StringOrNumber = string | number
 
+/**
+ * Boolean type from the string representation of boolean.
+ */
 export type StringToBoolean<T> = T extends 'true' | 'false' ? boolean : T
 
+/**
+ * String from any type.
+ */
 export type ToString<T> = T extends string ? T : string
 
+/**
+ * Record without the specified keys.
+ */
 export type Without<T, K> = Pick<T, Exclude<keyof T, K>>
 
+/**
+ * URL type.
+ */
 export type HTTPUrl = `http${string}`

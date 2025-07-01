e6d2e82879c48232665b686b86a860de81803ea8
Gabriele Canepa <git@gabrielecanepa.com>
2025-07-01 16:31:52 +0200

    ci: improve commitlint package scope detection using pnpm command

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,19 +1,28 @@
-import { readdirSync } from 'node:fs'
+import { execSync } from 'node:child_process'
 
 import { RuleConfigSeverity, type UserConfig } from '@commitlint/types'
 
-const SCOPES = ['deps', 'deps-dev', 'dev', 'infra', 'security']
+interface Package {
+  name: string
+  path: string
+  private: boolean
+  version: string
+}
 
-const packages = readdirSync('./packages', { withFileTypes: true })
-  .filter(dir => dir.isDirectory())
-  .map(dir => dir.name)
-  .flat()
+const NAMESPACE = '@repo'
+const LIST_CMD = 'pnpm m ls --depth -1 --json'
+const BASE_SCOPES = ['deps', 'deps-dev', 'dev', 'infra', 'security']
+
+const packages = JSON.parse(execSync(LIST_CMD, { encoding: 'utf8' })) as Package[]
+const packageNames = packages
+  .filter(pkg => pkg.name.startsWith(NAMESPACE))
+  .map((pkg: Package) => pkg.name.replace(`${NAMESPACE}/`, ''))
 
 export default {
   defaultIgnores: true,
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [RuleConfigSeverity.Disabled],
-    'scope-enum': [RuleConfigSeverity.Error, 'always', [...SCOPES, ...packages]],
+    'scope-enum': [RuleConfigSeverity.Error, 'always', [...packageNames, ...BASE_SCOPES]],
   },
 } satisfies UserConfig

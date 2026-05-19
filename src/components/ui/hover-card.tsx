c68e98b92a0dbd686077632c0c55c1012d07eab7
Gabriele Canepa <git@gabrielecanepa.com>
2026-05-20 00:32:58 +0100

    refactor: add icon picker translations and review data import

--- src/components/ui/hover-card.tsx
+++ src/components/ui/hover-card.tsx
@@ -29,7 +29,7 @@ export const HoverCardContent = ({
     <HoverCardPrimitive.Content
       align={align}
       className={cn(
-        'relative z-50 origin-(--radix-hover-card-content-transform-origin) rounded-lg border bg-popover p-3.5 text-popover-foreground shadow-xs outline-hidden data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95',
+        'relative z-50 origin-(--radix-hover-card-content-transform-origin) rounded-lg border bg-popover px-3.5 py-4 text-popover-foreground shadow-xs outline-hidden data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95',
         className
       )}
       data-slot="hover-card-content"
